f4539e2abe85596fb40b52d09c899269c9cbafe7
otaribo <126910299+otaribo@users.noreply.github.com>
2024-11-20 18:27:04 +0100

    20/11/2024

--- Java/UF1/Arrays/Parells de 0/Parellsde0.java
+++ Java/UF1/Arrays/Parells de 0/Parellsde0.java
@@ -1,9 +1,14 @@
 public class Parellsde0{
     public static void main(String[] args){
-        for(int i=0;i<100;i++){
-            if(i%2=0){
-                print(i);
+		int[] numerosParells = new int[101];
+        for(int i=(0);i<=200;i++){
+            if(i%2==0 || i==0){
+				numerosParells[i/2] = i;
             }
         }
+		for(int numero:numerosParells){
+			System.out.print(numero + " ");
+		}
+		System.out.println();
     }
 }
