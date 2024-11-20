8c32dc9a6427359b0d2ab813ea85cae8471c4e29
otaribo <126910299+otaribo@users.noreply.github.com>
2024-11-20 16:00:29 +0100

    20/11/2024

---
+++ Java/UF1/Arrays/Parells de 0/Parellsde0.java
@@ -0,0 +1,9 @@
+public class Parellsde0{
+    public static void main(String[] args){
+        for(int i=0;i<100;i++){
+            if(i%2=0){
+                print(i);
+            }
+        }
+    }
+}
