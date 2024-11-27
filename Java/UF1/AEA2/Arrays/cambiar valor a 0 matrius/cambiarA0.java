1f2901c2e50a5bd2ed594a909eed29dd8a336164
otaribo <oriolmarcostaribo@gmail.com>
2024-11-27 21:19:48 +0100

    27/11/2024

---
+++ Java/UF1/AEA2/Arrays/cambiar valor a 0 matrius/cambiarA0.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class cambiarA0{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int llargada_llista = scan.nextInt();
+        int[][] llista = new int[llargada_llista][5];
+        
+        for (int i = 0;i<llista.length;i++){
+            for(int j = 0;j<llista[i].length;j++){
+                llista[i][j] = i;
+                if(i%2==0){
+                    llista[i][j] = 0;
+                }
+                System.out.print(llista[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Java/UF1/AEA2/Arrays/cambiar_valor_a_1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class cambiar_valor_a_1{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int llargada_llista = scan.nextInt();
+        int[][] llista = new int[llargada_llista][5];
+        
+        for (int i = 0;i<llista.length;i++){
+            for(int j = 0;j<llista[i].length;j++){
+                if(i%2==0&&j%2==0){
+                    llista[i][j] = 1;
+                }
+                else{
+                    llista[i][j] = 0;
+                }
+                System.out.print("[" + llista[i][j] +"]");
+                System.out.print(" ");
+
+            }
+            System.out.println();
+        }
+    }
+}
