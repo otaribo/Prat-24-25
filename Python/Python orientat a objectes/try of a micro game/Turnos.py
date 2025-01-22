262af3175999ee25a3c8d43bf8d6fdf1b484b681
otaribo <oriolmarcostaribo@gmail.com>
2025-01-22 14:18:25 +0100

    22/01/2025

--- Python/Python orientat a objectes/try of a micro game/Turnos.py
+++ Python/Python orientat a objectes/try of a micro game/Turnos.py
@@ -12,41 +12,46 @@ class Partida():
         elif Seleccio == 2:
             self.CPU = Guerrero("Player",1000,10,15,7)
             self.Player = Escudero("CPU", 1000,5,12,9)
-        self.Inicar()
+        self.Iniciar(self.Player, self.CPU)
 
-    def Iniciar(self):
+    def Iniciar(self, Player, CPU):
         self.NumeroTurno = 0
         while self.Player.vida > 0 and self.CPU.vida > 0:
-            self.Turno()
+            self.Turno(Player, CPU)
 
-    def Turno(self):
-        self.Acciones(self.Player, self.AccionJugador)
-        self.Acciones(self.CPU, self.AccionCPU)
+    def Turno(self, Player, CPU):
+        self.Acciones(Player, self.AccionJugador)
+        self.Acciones(CPU, self.AccionCPU(self.CPU,self.Player))
         self.NumeroTurno += 1
 
-    def Acciones(self, Accion):
+    def Acciones(self, Player, Accion):
         if Accion ==1:
-            print(f"{self.nombre} ha atacado causando {self.daño} puntos de daño")
-            self.atacar
+            print(f"{Player.nombre} ha atacado causando {Player.daño} puntos de daño")
+            Player.atacar
         elif Accion == 2:
-            self.defender
+            Player.defender
         elif Accion == 3:
-            print(f"{self.nombre} se ha curado {self.curar} puntos de salud")
-            self.curarse
+            print(f"{Player.nombre} se ha curado {Player.curar} puntos de salud")
+            Player.curarse
     def AccionJugador():
         while(Accion != 1 or Accion != 2 or Accion != 3):
             Accion = int(input("[1] Atacar \n[2] Defender \n[3] Curarse"))
-        return Accion
-    def AccionCPU(self,Player):
-        if self.vida <= (self.vidaMax/100*30) and Player.vida > self.daño(Player):
+        respuesta = Accion
+        Accion=0
+        return respuesta
+    def AccionCPU(self, CPU, Player):
+        if CPU.vida <= (CPU.vidaMax/100*30) and Player.vida > CPU.daño(Player):
             Accion = 3
-        elif Player.vida > self.daño(Player):
+        elif Player.vida > CPU.daño(Player):
             Accion = 1
-        elif Player.vida < self.daño(Player):
+        elif Player.vida < CPU.daño(Player):
             Accion = 1
         else:
             Accion = 1
-        return Accion
+        respuesta = Accion
+        Accion=0
+        return respuesta
+        
 class Personaje:
     def __init__(self, nombre, vidaMax, ataque, defensa):
         self.nombre = nombre
