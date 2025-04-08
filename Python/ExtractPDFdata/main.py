4ccd4599a7399e818ad9c97ae5d97ff522d9e12c
otaribo <126910299+otaribo@users.noreply.github.com>
2025-04-09 00:02:06 +0200

    09/04/2025

--- Python/ExtractPDFdata/main.py
+++ Python/ExtractPDFdata/main.py
@@ -3,10 +3,11 @@ import os
 path = os.path.dirname(os.path.abspath(__file__))
 
 def pdf_to_txt(pdf_path, txt_path):
-    doc = fitz.open(pdf_path)
+    documento = fitz.open(pdf_path)
     texto_completo = ""
-    for pagina in doc:
+    for pagina in documento:
         texto_completo += pagina.get_text()
     with open(txt_path, 'w', encoding='utf-8') as archivo_txt:
         archivo_txt.write(texto_completo)
+
 pdf_to_txt(os.path.join(path,'data.pdf'), os.path.join(path, 'data.txt'))
