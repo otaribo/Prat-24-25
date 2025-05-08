6e555005519a7604fcb7e69143cb3ac10774867c
otaribo <126910299+otaribo@users.noreply.github.com>
2025-05-08 23:40:10 +0200

    08/05/2025

--- Java/AEA4/InvertirParaules/main.java
+++ Java/AEA4/InvertirParaules/main.java
@@ -5,43 +5,46 @@ import java.io.*;
 public class main {
     public static void main(String[] args) {
         File path = null;
+        //trobem la path base del fitxer (Sol la troba si el document s'executa desde terminal!!!!)
         try{
             path = new File(main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-            
-            System.out.println("Path: " + path.getParent());
         }
-        catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        catch (Exception e) {}
 
         String abspath = path.getParent().replace(".", "");
 
+        //Especifiquem la path dels arxius d'entrada i sortida.
         String InputFilePath = abspath + "input.txt";
         String OutputFilePath = abspath + "output.txt";
         
         BufferedReader lector = null;
         FileWriter writer = null;
         
+        //Si, no es com has explicat pero com no estaba en aquella clase m'he buscat la vida. Basicament creem un lector que llegeix el document de entrada i un escriptor que escriura al document de sortida.
         try {
             lector = new BufferedReader(new FileReader(InputFilePath));
             writer = new FileWriter(OutputFilePath);
 
+            //Una variable on emmagetzarem la linea que estem llegint del document d'entrada.
             String linia;
             
-            // Llegim cada línia del fitxer d'entrada
+            //un bucle que es repeteix mentres quedin caracter que llegir al document.
             while ((linia = lector.readLine()) != null) {
-                char[] arrayLinea = linia.toCharArray();
-                for(int i = 0; i<arrayLinea.length;i++){
-                    writer.write(arrayLinea[arrayLinea.length-1-i]);
+                //Pasem el String linia a un array de chars.
+                char[] arrayLinia = linia.toCharArray();
+                for(int i = 0; i<arrayLinia.length;i++){
+                    //Escribim el caracter del costat oposat.
+                    writer.write(arrayLinia[arrayLinia.length-1-i]);
                 }
+                //al acabar la linea en la que estem System.lineSeparator fa un enter per escriure a la següent linia
                 writer.write(System.lineSeparator());
             }
-            
-            System.out.println("Fitxer processat correctament. Resultat guardat a " + OutputFilePath);
-            
+            System.out.println("fitxer posat a reversa correctament " + OutputFilePath);
+
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         } finally {
+            //Intentem tancar el lector i el escriptor
             try{
                 lector.close();
                 writer.close();
