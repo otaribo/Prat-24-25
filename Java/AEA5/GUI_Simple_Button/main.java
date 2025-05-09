952c00c9ae0656c43f9a979baf4e4a6abb218914
otaribo <oriolmarcostaribo@gmail.com>
2025-05-09 16:27:19 +0200

    09/05/2025

---
+++ Java/AEA5/GUI_Simple_Button/main.java
@@ -0,0 +1,10 @@
+package Java.AEA5.GUI_Simple_Button;
+
+import Java.AEA5.UI;
+
+public class main{
+    UI ui = new UI();
+    public void main(String[] args) {
+        ui.ventana(500,500);
+    }
+}
---
+++ Java/AEA5/UI.java
@@ -0,0 +1,8 @@
+package Java.AEA5;
+import javax.swing.JFrame;
+
+public class UI extends JFrame{
+    public void ventana(int height, int width){
+        this.setSize(height, width);
+    }
+}
